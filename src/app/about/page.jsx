455416fb0130a0e3528811afb0796d59a1a21240
abdulrg <abdulrg01@gmail.com>
2023-02-28 10:59:40 +0000

    Adding about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-10 lg:mx-32 space-y-4 lg:space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About Page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Inventore eaque ratione reprehenderit nulla reiciendis 
+            non natus omnis ut modi similique.
+        </p>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Amet, officia. Dignissimos delectus, molestiae eveniet 
+            blanditiis quis voluptate maiores ducimus labore!
+        </p>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Suscipit atque minus amet repellendus consequuntur error, 
+            non magnam beatae voluptates deleniti rerum modi perferendis 
+            voluptatem incidunt temporibus architecto quidem voluptate praesentium. Incidunt, pariatur mollitia. Sunt iure deserunt reprehenderit eaque vero! Voluptas.
+        </p>
+    </div>
+  )
+}
