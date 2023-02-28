292a68c5c761e9e766d354cf27098bf4c8e6f71b
abdulrg <abdulrg01@gmail.com>
2023-02-28 16:53:10 +0000

    create the navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='max-w-6xl mx-10 lg:mx-32 space-y-4 lg:space-y-4'>
+    <div className='max-w-6xl mx-7 lg:mx-32 space-y-4'>
         <h1 className='text-2xl font-medium text-amber-600'>About Page</h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -10,15 +10,17 @@ export default function page() {
             non natus omnis ut modi similique.
         </p>
         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Amet, officia. Dignissimos delectus, molestiae eveniet 
-            blanditiis quis voluptate maiores ducimus labore!
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          In veritatis laudantium, atque assumenda provident eum veniam 
+          labore cum sint facere iste quibusdam minima repudiandae perspiciatis 
+          distinctio nisi quia quae eveniet, fugit natus officiis fuga. Ipsa aut molestias fuga quidem nulla.
         </p>
 
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Suscipit atque minus amet repellendus consequuntur error, 
-            non magnam beatae voluptates deleniti rerum modi perferendis 
-            voluptatem incidunt temporibus architecto quidem voluptate praesentium. Incidunt, pariatur mollitia. Sunt iure deserunt reprehenderit eaque vero! Voluptas.
+          Perferendis illo repudiandae, non eius voluptate cupiditate laboriosam praesentium voluptatem soluta perspiciatis asperiores quos 
+          fuga nihil facilis recusandae exercitationem ipsa id maxime magni modi. Quidem labore magni nulla explicabo 
+          obcaecati porro! Quis, corporis nam dolor quae eius natus impedit repellat expedita suscipit, cumque beatae! Saepe, assumenda quis hic officia maxime omnis itaque, 
+          blanditiis vitae harum aut, sunt suscipit! Accusantium in fuga nostrum officiis placeat dignissimos esse odio unde harum quam. Qui, itaque?
         </p>
     </div>
   )
