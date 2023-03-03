c92ee8db2e48ba1040ebe26f8519a19f0a0b6d8b
abdulrg <abdulrg01@gmail.com>
2023-03-03 17:18:40 +0000

    add card component

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental:{ appDir: true },
-  reactStrictMode: false,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["image.tmdb.org"]
+  }
 }
 
 module.exports = nextConfig
